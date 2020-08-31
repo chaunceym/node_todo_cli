775e8399c5c46a59683b1696632e6e42b2d50d64
mxylovezh <18322276773@163.com>
2020-08-31 15:22:06 +0800

    finish the write until test with jest

--- __test__/db.spec.js
+++ __test__/db.spec.js
@@ -5,8 +5,18 @@ jest.mock('fs')
 describe('db', () => {
   it('read function', async () => {
     // 假数据测试代码
-    fs.setMock('/xxx', null, JSON.stringify([{'title': 'eat noddle', done: true}]))
+    fs.setReadMock('/xxx', null, JSON.stringify([{'title': 'eat noddle', done: true}]))
     const list = await db.read('/xxx')
     expect(list).toStrictEqual([{'title': 'eat noddle', done: true}])
+  })
+  it('write function', async () => {
+    let fakeFile
+    fs.setWriteMock('/yyy',(path,data,callback)=>{
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{title: 'meng',done: true},{title: 'meng',done: true}]
+    await db.write(list,'/yyy')
+    expect(fakeFile).toBe(JSON.stringify(list))
   })
 })
